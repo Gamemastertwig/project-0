9556bd5d210d8d724618e8d69405222cf9f06e21
Brandon Locker <brandonlocker@hotmail.com>
2020-01-07 14:52:36 -0600

    Updated to include inputs
    Added ability to get user's character name
    Added ability to get user's class

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
-// Project-O 
+// Project-O
 // Character Generator
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Main Function - This is where it all starts!
 func main() {
-	println("This is the core to my first GO project: CODENAME Project-0")
+	var n string
+	fmt.Println("What is your Character's Name?")
+	fmt.Scanln(&n)
+	fmt.Println("Hello, " + n)
+	fmt.Println("What is your class? [T]hief, [M]age, or [W]arrior")
+	var c string
+	fmt.Scan(&c)
+	c = strings.ToLower(c)
+	if c == "t" || c == "thief" {
+		fmt.Println(n + " is a Thief!")
+	} else if c == "m" || c == "mage" {
+		fmt.Println(n + " is a Mage!")
+	} else if c == "w" || c == "warrior" {
+		fmt.Println(n + " is a Warrior!")
+	} else {
+		fmt.Println("invalid class selected...")
+	}
 }
