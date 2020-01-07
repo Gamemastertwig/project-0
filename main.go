3943281e2399fd94401a557b83fea098144a8d93
Brandon Locker <brandonlocker@hotmail.com>
2020-01-07 13:24:35 -0600

    Added main.go (simple working)
    Commited from windows envorment

---
+++ main.go
@@ -0,0 +1,8 @@
+// Project-O 
+// Character Generator
+package main
+
+// Main Function - This is where it all starts!
+func main() {
+	println("This is the core to my first GO project: CODENAME Project-0")
+}
