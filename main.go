d6b008f4cec946cf70d1fa3d1e507e2faacc9e8e
Brandon Locker <brandonlocker@hotmail.com>
2020-01-08 16:10:02 -0600

    Functionalization!
    moved data to struc and added funcs to add support

--- main.go
+++ main.go
@@ -7,13 +7,34 @@ import (
 	"strings"
 )
 
-// Main Function - This is where it all starts!
-func main() {
+type npc struct {
+	name  string
+	class string
+	stat  int
+}
+
+var minion npc
+
+func inti() {
+
+}
+
+func setName(name string) {
+	minion.name = name
+}
+
+func setControlledName() {
 	var n string
 	fmt.Println("What is your Character's Name?")
 	fmt.Scanln(&n)
-	fmt.Println("Hello, " + n)
+	minion.name = n
+}
 
+func setClass(class string) {
+	minion.class = class
+}
+
+func setControlledClass() {
 	a := false
 	for a == false {
 		fmt.Println("What is your class? [T]hief, [M]age, or [W]arrior")
@@ -21,16 +42,28 @@ func main() {
 		fmt.Scan(&c)
 		c = strings.ToLower(c)
 		if c == "t" || c == "thief" {
-			fmt.Println(n + " is a Thief!")
+			minion.class = "Thief"
 			a = true
 		} else if c == "m" || c == "mage" {
-			fmt.Println(n + " is a Mage!")
+			minion.class = "Mage"
 			a = true
 		} else if c == "w" || c == "warrior" {
-			fmt.Println(n + " is a Warrior!")
+			minion.class = "Warrior"
 			a = true
 		} else {
 			fmt.Println("invalid class selected...")
 		}
 	}
+}
+
+func setStat(stat int) {
+	minion.stat = stat
+}
+
+// Main Function - This is where it all starts!
+func main() {
+	setControlledName()
+	setControlledClass()
+
+	fmt.Println(minion)
 }
