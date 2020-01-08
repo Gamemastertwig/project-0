5aa02515354d5156e2c112b45e7c0d166d33543a
Brandon Locker <brandonlocker@hotmail.com>
2020-01-08 15:12:34 -0600

    Added loop to request class
    continues until valid option is give

--- main.go
+++ main.go
@@ -13,17 +13,24 @@ func main() {
 	fmt.Println("What is your Character's Name?")
 	fmt.Scanln(&n)
 	fmt.Println("Hello, " + n)
-	fmt.Println("What is your class? [T]hief, [M]age, or [W]arrior")
-	var c string
-	fmt.Scan(&c)
-	c = strings.ToLower(c)
-	if c == "t" || c == "thief" {
-		fmt.Println(n + " is a Thief!")
-	} else if c == "m" || c == "mage" {
-		fmt.Println(n + " is a Mage!")
-	} else if c == "w" || c == "warrior" {
-		fmt.Println(n + " is a Warrior!")
-	} else {
-		fmt.Println("invalid class selected...")
+
+	a := false
+	for a == false {
+		fmt.Println("What is your class? [T]hief, [M]age, or [W]arrior")
+		var c string
+		fmt.Scan(&c)
+		c = strings.ToLower(c)
+		if c == "t" || c == "thief" {
+			fmt.Println(n + " is a Thief!")
+			a = true
+		} else if c == "m" || c == "mage" {
+			fmt.Println(n + " is a Mage!")
+			a = true
+		} else if c == "w" || c == "warrior" {
+			fmt.Println(n + " is a Warrior!")
+			a = true
+		} else {
+			fmt.Println("invalid class selected...")
+		}
 	}
 }
